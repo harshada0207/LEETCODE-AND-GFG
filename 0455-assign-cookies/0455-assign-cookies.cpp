9d5e88cf764f60a66cce99fbc0f7fee17a63c779
HARSHADA KORANNE <95358157+tanu0207@users.noreply.github.com>
2024-04-05 16:39:56 +0530

    Time: 19 ms (74.19%), Space: 20.5 MB (88.58%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(s.begin(),s.end());
+        sort(g.begin(),g.end());
+        int i=0;
+        int j=0;
+        int count=0;
+        while(j<s.size() && i<g.size())
+        {
+            if(s[j]>=g[i])
+            {
+                count++;
+                i++;
+                
+                
+                
+            }
+            j++;
+            
+        }
+
+        return count;
+        
+    }
+};
