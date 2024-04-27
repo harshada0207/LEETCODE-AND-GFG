4be559e42e31978658d6c383e04aca449ef2c842
HARSHADA KORANNE <95358157+tanu0207@users.noreply.github.com>
2024-04-27 22:38:39 +0530

    Time: 781 ms (18.83%), Space: 10.7 MB (81.74%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,24 +1,30 @@
 class Solution {
-public:
-    int count=0;
-    void solve(vector<int>& nums, int target,int sum,int ind)
-    {
-        if(ind==nums.size())
+    private:
+    int solve(vector<int>& nums, int target,int ind,int sum)
+    {   
+       if(ind<0)
+       {
+        if(sum==target)
         {
-            if(sum==target)
-            {
-                  count++;
-
-            }
-            return;
+            return 1;
         }
-        solve(nums,target,sum+nums[ind],ind+1);
-        solve(nums,target,sum-nums[ind],ind+1);
+        else
+        {
+            return 0;
+        }
+       }
+      
+       int left = solve(nums,target,ind-1,sum-nums[ind]);
+       int right = solve(nums,target,ind-1,sum+nums[ind]);
+
+       return left+right;
+      
     }
+public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum=0;
-        int ind=0;
-        solve(nums,target,sum,ind);
-        return count;
+        int n = nums.size();
+        int sum = 0;
+        int ans = solve(nums,target,n-1,sum);
+        return ans;
     }
 };
