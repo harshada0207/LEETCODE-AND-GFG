9594b54800b4ab702a977c171cd99e2adf926b5d
HARSHADA KORANNE <95358157+tanu0207@users.noreply.github.com>
2024-06-24 21:18:45 +0530

    Time: 4 ms (94.61%), Space: 12.6 MB (94.31%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,33 +1,51 @@
 class Solution {
 public:
    
-    int solve(vector<int>&nums,int ind,int prev_ind, vector<vector<int>>&dp)
-    {
-        if(ind==nums.size())
-        {
-            return 0;
-        }
-       if(dp[ind][prev_ind+1]!=-1)
-       {
-          return dp[ind][prev_ind+1];
-       }
-       int take= INT_MIN;
-       if(prev_ind==-1 ||nums[ind]>nums[prev_ind])
-       {
-          take=1+solve(nums,ind+1,ind,dp);
-       }
+    // int solve(vector<int>&nums,int ind,int prev_ind, vector<vector<int>>&dp)
+    // {
+    //     if(ind==nums.size())
+    //     {
+    //         return 0;
+    //     }
+    //    if(dp[ind][prev_ind+1]!=-1)
+    //    {
+    //       return dp[ind][prev_ind+1];
+    //    }
+    //    int take= INT_MIN;
+    //    if(prev_ind==-1 ||nums[ind]>nums[prev_ind])
+    //    {
+    //       take=1+solve(nums,ind+1,ind,dp);
+    //    }
      
-       int not_take=solve(nums,ind+1,prev_ind,dp);
+    //    int not_take=solve(nums,ind+1,prev_ind,dp);
        
-       return dp[ind][prev_ind+1]= max(take,not_take);
+    //    return dp[ind][prev_ind+1]= max(take,not_take);
         
 
        
-    }
+    // }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return solve(nums,0,-1,dp);
+        vector<int>ans;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(ans.empty())
+            {
+                ans.push_back(nums[i]);
+                
+            }
+            else if(!ans.empty() && nums[i]>ans.back())
+            {
+                ans.push_back(nums[i]);
+            }
+            else
+            {
+                  int ind=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+                  ans[ind]=nums[i];
+            }
+        }
+        return ans.size();
         
         
     }
