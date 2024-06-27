3c12bdf7b07233dbc582c0c0c4b906a4436a6403
HARSHADA KORANNE <95358157+harshada0207@users.noreply.github.com>
2024-06-27 21:36:32 +0530

    Time: 81 ms (58.95%), Space: 67.3 MB (8.77%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n=arr.size();
+        vector<int>dp1(n,1);
+        vector<int>dp2(n,1);
+        for(int i=1;i<n;i++)
+        {
+            
+                if(arr[i]>arr[i-1])
+                {
+                    dp1[i]=max(dp1[i],dp1[i-1]+1);
+                }
+            
+        }
+         for(int i=n-2;i>=0;i--)
+        {
+            
+                if(arr[i]>arr[i+1])
+                {
+                    dp2[i]=max(dp2[i],dp2[i+1]+1);
+                }
+            
+        }
+        int maxi=0;
+        for(int i=0;i<n;i++)
+        {
+            if(dp1[i]>1&&dp2[i]>1)
+            {
+                return i;
+
+            }
+        }
+      
+        return -1;
+    }
+};
