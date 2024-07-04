dbb2cb41309de0e92d81aaafc51adbd3b9d96218
HARSHADA KORANNE <95358157+harshada0207@users.noreply.github.com>
2024-07-04 11:31:07 +0530

    Time: 1022 ms (18.72%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT w1.id
+FROM weather AS w1
+WHERE w1.temperature > (
+    SELECT w2.temperature
+    FROM weather AS w2
+    WHERE DATEDIFF(w1.recordDate,w2.recordDate)=1
+);
